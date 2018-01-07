4e3d9231e6c51414cd6d23b0f40cff7b925d2092
Mario Bragg <mbragg@xevo.com>
2018-01-07 00:31:34 -0500

    Intrgrate Buld Buddy SDK into XevoAR

--- ARKitPOC/ARKitPOC/AppDelegate.swift
+++ ARKitPOC/ARKitPOC/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import ARKit
+import BuddyBuildSDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         
+        BuddyBuildSDK.setup()
         setDefaultsFromSettingsBundle()
         
         guard ARWorldTrackingConfiguration.isSupported else {
